799efc6521d3840fd7c66cf8e59abe34b0c66114
Linda Speight <linda@reachmaine.com>
2016-10-10 16:21:32 -0400

    Remove comments from single posts & recent posts

---
+++ oshin_child/blog/post-top-details.php
@@ -0,0 +1,12 @@
+<?php /* mods 
+* 10Oct16 zig - dont show 
+*/ ?>
+<?php global $meta_sep; ?>
+<nav class="post-nav meta-font secondary_text">
+	<ul class="clearfix">
+		<?php /* zig xout <li class="post-meta post-comments">
+			<a href="<?php comments_link(); ?>"><?php comments_number('0','1','%');?> <?php _e(' comments','be-themes'); ?></a><span class="post-meta-sep">/</span>
+		</li> */ ?>
+		<li class="post-meta"><?php echo get_the_date(); ?></li>
+	</ul>
+</nav>
