0f265d6befb04d0b84d9946f03e52165955679cc
Linda Speight <linda@reachmaine.com>
2016-10-20 09:57:34 -0400

    adding author box to single blog post.
    first pass

--- oshin_child/single.php
+++ oshin_child/single.php
@@ -1,12 +1,13 @@
 <?php
 /*
  *  The template for displaying a Blog Post.
- * 
+ *
  * mods:
- *   17Oct16 zig - Add post navigation 
+ *   17Oct16 zig - Add post navigation
+ *	20Oct16 zig - add Author meta box
 */
 get_header();
-global $be_themes_data; 
+global $be_themes_data;
 $sidebar_flag = (array_key_exists('blog_single_sidebar', $be_themes_data) ) ?  $be_themes_data['blog_single_sidebar'] : '1' ;
 $blog_style = ((!isset($be_themes_data['blog_style'])) || empty($be_themes_data['blog_style'])) ? 'style1' : $be_themes_data['blog_style'];
 $sidebar = 'right';
@@ -15,7 +16,7 @@ $content_single_sidebar = ( isset($sidebar_flag) && '1' != $sidebar_flag) ? '' :
 $enable_breadcrumb = ( isset($be_themes_data['enable_breadcrumb']) && 1 == $be_themes_data['enable_breadcrumb']) ? 1 : 0;
 
 while ( have_posts() ) : the_post(); ?>
-	<?php 
+	<?php
 	if($enable_breadcrumb){
 		get_template_part( 'page-blogpost-breadcrumb' );
 	}?>
@@ -28,6 +29,33 @@ while ( have_posts() ) : the_post(); ?>
 						get_template_part( 'blog/loop', $blog_style );
 					?>
 				</div> <!--  End Page Content -->
+				<div id="author-box">
+					<div class="be-section-pad">
+						<div class="be-row be-wrap be-no-space ">
+							<div class="one-fifth column-block">
+								<?php
+									$user = get_the_author_meta('ID');
+									echo get_avatar($user,120);
+								?>
+							</div>
+							<div class="two-third column-block">
+								<h6 class="author-name uppercase">
+									<?php echo esc_html( get_the_author_meta( 'display_name' ) );?>
+								</h6>
+
+								<?php if(get_the_author_meta('yim')){?>
+								<p class="author-title"><?php echo esc_html(get_the_author_meta('yim')); ?></p>
+								<?php }?>
+
+								<p class="author-desc"><?php echo esc_html(get_the_author_meta('description'));?></p>
+
+								<?php if(get_the_author_meta('aim')){?>
+								<p class="author-twitter"><a href="http://twitter.com/<?php echo get_the_author_meta('aim');?>"><?php echo esc_html(get_the_author_meta('aim'));?></a></p>
+								<?php }?>
+							</div> <!--end -->
+						</div> <!-- end row -->
+					</div> <!-- end section -->
+				</div> <!-- end author box -->
 				<!-- post navigation -->
 				<div class="post-navigation">
 					<!--h4 class="aligncenter">Additional Posts</h4-->
@@ -45,7 +73,7 @@ while ( have_posts() ) : the_post(); ?>
 				</section>
 			<?php } ?>
 		</div>
-	</section> <?php 
+	</section> <?php
 endwhile;
-get_footer(); 
+get_footer();
 ?>
