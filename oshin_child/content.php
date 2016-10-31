3934fa27f242aa5abb50044fe7d16f1416e2f0ec
Linda Speight <linda@reachmaine.com>
2016-10-31 15:35:06 -0400

    image size when not single.
    use medium

--- oshin_child/content.php
+++ oshin_child/content.php
@@ -1,12 +1,19 @@
-<?php 
+<?php
+/* mods
+* 31 Oct16 zig - if not single use medium image size.
+*/
 global $be_themes_data, $blog_attr;
 if( has_post_thumbnail() ) :
-	$blog_image_size = 'blog-image';
-    if( $blog_attr['style'] == 'style3' ) {
-    	$blog_image_size = 'portfolio-masonry';
-    }
+	if(!is_single()){
+		$blog_image_size = 'medium'; }
+	else {
+		$blog_image_size = 'blog-image';
+	    if( $blog_attr['style'] == 'style3' ) {
+	    	$blog_image_size = 'portfolio-masonry';
+	    }
+	}
 	$thumb = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), $blog_image_size );
-    $thumb_full = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full' );    
+    $thumb_full = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full' );
 	$url = $thumb['0'];
 	$attachment_full_url = $thumb_full[0];
 	$link = $attachment_full_url;
@@ -16,17 +23,17 @@ if((isset($be_themes_data['open_to_lightbox']) && 1 == $be_themes_data['open_to_
 	$class = 'image-popup-vertical-fit mfp-image';
 } else {
 	if(!is_single()){
-		$link = get_permalink();	
+		$link = get_permalink();
 	}else{
 		$link = '#';
 	}
 }
-if( !empty( $url )  ) { 
+if( !empty( $url )  ) {
 	if (!is_single()  || (get_post_meta(get_the_ID(), 'reach_disable_thumb', true)  != 'yes')  )  {
 
 ?>
-<div class="post-thumb">	
-	<div class="">        	
+<div class="post-thumb">
+	<div class="">
 		<a href="<?php echo esc_url( $link ) ?>" class="<?php echo $class; ?> thumb-wrap"><?php the_post_thumbnail( $blog_image_size ); ?>
 			<div class="thumb-overlay">
 				<div class="thumb-bg">
@@ -36,6 +43,6 @@ if( !empty( $url )  ) {
 				</div>
 			</div>
 		</a>
-	</div>			
+	</div>
 </div>
 <?php  } } ?>
